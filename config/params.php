3e3e3c7946406898fbec5fe292d0c0902d9b1fea
rikosage <sageriko@gmail.com>
2017-07-27 22:31:15 +0300

    Added sms notify

--- config/params.php
+++ config/params.php
@@ -2,4 +2,5 @@
 
 return [
     'adminEmail' => 'admin@example.com',
+    'sitename' => "Infotech Test",
 ];
--- notifiers/SmsNotifier.php
+++ notifiers/SmsNotifier.php
@@ -2,6 +2,8 @@
 
 namespace app\notifiers;
 
+
+use Yii;
 use app\components\BaseNotifier;
 
 /**
@@ -10,11 +12,65 @@ use app\components\BaseNotifier;
 class SmsNotifier extends BaseNotifier
 {
     /**
+     * Объект-обертка над CURL
+     * @var \linslin\yii2\curl\Curl;
+     */
+    protected $curl;
+
+    /**
+     * Адрес, куда стучаться
+     * @var string
+     */
+    public $apiAddress = "http://smspilot.ru/api.php";
+
+    /**
+     * Ключ API
+     * @var string
+     */
+    public $apiKey = "XXXXXXXXXXXXYYYYYYYYYYYYZZZZZZZZXXXXXXXXXXXXYYYYYYYYYYYYZZZZZZZZ";
+
+    /**
+     * Предпочтительный формат ответа
+     * @var [type]
+     */
+    public $formatResponse = "json";
+
+
+    /**
+     * @inheritDoc
+     */
+    public function init()
+    {
+        $this->curl = new \linslin\yii2\curl\Curl;
+    }
+
+    /**
+     * API Принимает телефон только в виде цифр.
+     * Преобразовываем телефон к нужному формату
+     * @return void
+     */
+    protected function normalizePhone()
+    {
+        $this->target = preg_replace("/\D/", "", $this->target);
+    }
+
+    /**
      * @inheritDoc
      */
     public function send() : bool
     {
-        // Отправляем sms пользователю
-        return true;
+        $this->normalizePhone();
+        $response = $this->curl->setGetParams([
+            'send' => $this->message,
+            'to' => $this->target,
+            "from" => Yii::$app->params['sitename'],
+            "apiKey" => $this->apiKey,
+            'format' => $this->formatResponse,
+         ])
+        ->get($this->apiAddress);
+
+        $response = json_decode($response);
+
+        return !(bool) $response->error;
     }
 }
